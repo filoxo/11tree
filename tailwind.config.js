ec40359be573cfb56487f0e98d1b19c123806809
Carlos Filoteo <c.filoteo12@gmail.com>
2024-03-27 17:37:32 -0600

    Capture classes in eleventy.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{html,js,md,njk}"],
+  content: ["./src/**/*.{html,js,md,njk}", "./.eleventy.js"],
   plugins: [require("@tailwindcss/typography")],
 };
