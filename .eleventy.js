d666993365f7d94b68963857d556044e08a07946
Carlos Filoteo <c.filoteo12@gmail.com>
2024-04-14 21:49:40 -0600

    Install @11ty/eleventy-plugin-webc

--- .eleventy.js
+++ .eleventy.js
@@ -1,58 +1,61 @@
 // const eleventySass = require("eleventy-sass");
 const PostCSSPlugin = require("eleventy-plugin-postcss");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
   eleventyConfig.addPlugin(PostCSSPlugin);
-  eleventyConfig.addShortcode("avatar", function (url, alt) {
-    return `<img src="${url}" alt="${alt}" class="rounded-full w-24 h-24 border-4 border-solid border-teal-500 m-auto" />`;
-  });
+  eleventyConfig.addPlugin(pluginWebc);
 
-  eleventyConfig.addPairedShortcode(
-    "link",
-    function (content, href, options = {}) {
-      const external = options.external || true;
-      const props = {
-        href,
-        class: "inline-flex items-center gap-4 justify-center",
-        target: external ? "_blank" : undefined,
-        rel: external ? "noopener noreferrer" : undefined,
-        "aria-label": options["aria-label"],
-      };
+  // eleventyConfig.addShortcode("avatar", function (url, alt) {
+  //   return `<img src="${url}" alt="${alt}" class="rounded-full w-24 h-24 border-4 border-solid border-teal-500 m-auto" />`;
+  // });
 
-      // convert to `attribute="value"`
-      const linkAttrs = Object.entries(props)
-        .map(([attr, value]) => (value ? `${attr}="${value}"` : ""))
-        .join(" ");
+  //   eleventyConfig.addPairedShortcode(
+  //     "link",
+  //     function (content, href, options = {}) {
+  //       const external = options.external || true;
+  //       const props = {
+  //         href,
+  //         class: "inline-flex items-center gap-4 justify-center",
+  //         target: external ? "_blank" : undefined,
+  //         rel: external ? "noopener noreferrer" : undefined,
+  //         "aria-label": options["aria-label"],
+  //       };
 
-      return `<a ${linkAttrs}>${content}</a>`;
-    }
-  );
+  //       // convert to `attribute="value"`
+  //       const linkAttrs = Object.entries(props)
+  //         .map(([attr, value]) => (value ? `${attr}="${value}"` : ""))
+  //         .join(" ");
 
-  let linkIdCount = 0;
-  const genLinkId = () => `link-${linkIdCount++}`;
+  //       return `<a ${linkAttrs}>${content}</a>`;
+  //     }
+  //   );
 
-  eleventyConfig.addPairedShortcode("nsfw", function (content, href) {
-    const id = genLinkId();
-    const props = {
-      href,
-      class: "inline-flex items-center gap-4 justify-center",
-      target: "_blank",
-      rel: "noopener noreferrer",
-    };
+  //   let linkIdCount = 0;
+  //   const genLinkId = () => `link-${linkIdCount++}`;
 
-    // convert to `attribute="value"`
-    const linkAttrs = Object.entries(props)
-      .map(([attr, value]) => (value ? `${attr}="${value}"` : ""))
-      .join(" ");
+  //   eleventyConfig.addPairedShortcode("nsfw", function (content, href) {
+  //     const id = genLinkId();
+  //     const props = {
+  //       href,
+  //       class: "inline-flex items-center gap-4 justify-center",
+  //       target: "_blank",
+  //       rel: "noopener noreferrer",
+  //     };
 
-    return `<button ${linkAttrs} onClick="document.getElementById('${id}')?.showModal()" type="button">${content}</button>
-<dialog id="${id}" class="p-6 md:p-8 space-y-4 rounded-xl shadow backdrop-blur-lg bg-white/90">
-  <p class="text-center">Are you 18 years or older?</p>
-  <button onClick="document.getElementById('${id}')?.close()">No, I'm not 18</button>
-  <a ${linkAttrs}>Yes, proceed to site</a>
-</dialog>`;
-  });
+  //     // convert to `attribute="value"`
+  //     const linkAttrs = Object.entries(props)
+  //       .map(([attr, value]) => (value ? `${attr}="${value}"` : ""))
+  //       .join(" ");
+
+  //     return `<button ${linkAttrs} onClick="document.getElementById('${id}')?.showModal()" type="button">${content}</button>
+  // <dialog id="${id}" class="p-6 md:p-8 space-y-4 rounded-xl shadow backdrop-blur-lg bg-white/90">
+  //   <p class="text-center">Are you 18 years or older?</p>
+  //   <button onClick="document.getElementById('${id}')?.close()">No, I'm not 18</button>
+  //   <a ${linkAttrs}>Yes, proceed to site</a>
+  // </dialog>`;
+  //   });
 
   eleventyConfig.addNunjucksGlobal("currentYear", new Date().getFullYear());
   eleventyConfig.addNunjucksGlobal("author", "Your name here");
