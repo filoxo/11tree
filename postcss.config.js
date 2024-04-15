57feaeeb7b7c4ff5ebfb544043d67594f376b3e9
Carlos Filoteo <c.filoteo12@gmail.com>
2024-04-15 14:52:40 -0600

    Minify styles in prod build

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+module.exports = ({ env }) => ({
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
     "postcss-import": {},
+    "@csstools/postcss-minify": env === "production" ? {} : false,
   },
-};
+});
