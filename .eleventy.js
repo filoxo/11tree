614b8b7478efc00c5d6ce39d3808f712680aba02
Carlos Filoteo <c.filoteo12@gmail.com>
2024-04-15 00:14:02 -0600

    Remove icon functionality; add globalData

--- .eleventy.js
+++ .eleventy.js
@@ -63,32 +63,31 @@ module.exports = function (eleventyConfig) {
   // </dialog>`;
   //   });
 
-  eleventyConfig.addNunjucksGlobal("currentYear", new Date().getFullYear());
-  eleventyConfig.addNunjucksGlobal("author", "Your name here");
+  eleventyConfig.addGlobalData("currentYear", new Date().getFullYear());
+  eleventyConfig.addGlobalData("author", "Your name here");
+  eleventyConfig.addGlobalData("title", "Your site title here");
 
-  // selected icon theme prefix, https://iconify.design/icon-sets/
-  const iconPrefix = "ion";
-  // accumulate which icons are being used on site
-  const iconSet = new Set();
+  // // selected icon theme prefix, https://iconify.design/icon-sets/
+  // const iconPrefix = "ion";
+  // // accumulate which icons are being used on site
+  // const iconSet = new Set();
 
-  /* use in .njk files, `{% icon 'icon-name' %}` */
-  eleventyConfig.addShortcode("icon", (iconName) => {
-    iconSet.add(iconName);
-    return `<span class="icon icon--${iconPrefix} icon--${iconPrefix}--${iconName} text-current text-3xl" aria-hidden="true"></span>`;
-  });
+  // /* use in .njk files, `{% icon 'icon-name' %}` */
+  // eleventyConfig.addShortcode("icon", (iconName) => {
+  //   iconSet.add(iconName);
+  //   return `<span class="icon icon--${iconPrefix} icon--${iconPrefix}--${iconName} text-current text-3xl" aria-hidden="true"></span>`;
+  // });
 
-  // using https://iconify.design/docs/usage/css/no-code/ approach for icons.
-  // This generates an Iconify stylesheet URL, exposed as a njk global for interpolation in default layout.
-  eleventyConfig.addNunjucksGlobal("iconSetUrl", () => {
-    const icons = encodeURIComponent(Array.from(iconSet).join(","));
-    // enumerate icons from https://icon-sets.iconify.design/ri/ in order to optimize stylesheet weight
-    const iconUrl = `https://api.iconify.design/${iconPrefix}.css?icons=${icons}`;
-    return iconUrl;
-  });
+  // // using https://iconify.design/docs/usage/css/no-code/ approach for icons.
+  // // This generates an Iconify stylesheet URL, exposed as a njk global for interpolation in default layout.
+  // eleventyConfig.addGlobalData("iconSetUrl", () => {
+  //   const icons = encodeURIComponent(Array.from(iconSet).join(","));
+  //   // enumerate icons from https://icon-sets.iconify.design/ri/ in order to optimize stylesheet weight
+  //   const iconUrl = `https://api.iconify.design/${iconPrefix}.css?icons=${icons}`;
+  //   return iconUrl;
+  // });
 
   return {
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "dist",
