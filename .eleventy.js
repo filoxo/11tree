2a8443df639e6e30e206c88a69ce96bd8e70760d
Carlos Filoteo <c.filoteo12@gmail.com>
2024-04-19 22:31:46 -0600

    Add html minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 // const eleventySass = require("eleventy-sass");
 const PostCSSPlugin = require("eleventy-plugin-postcss");
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const htmlminifier = require("html-minifier-terser");
+
+const IS_PROD_BUILD = process.env.NODE_ENV === "production";
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
@@ -11,6 +14,30 @@ module.exports = function (eleventyConfig) {
      * @see https://www.11ty.dev/docs/languages/webc/#global-no-import-components
      */
     components: "src/_components/**/*.webc",
+  });
+
+  eleventyConfig.addTransform("htmlminifier", (content, outputPath) => {
+    if (!outputPath.endsWith(".html")) return content;
+
+    return htmlminifier.minify(
+      content,
+      IS_PROD_BUILD
+        ? {
+            useShortDoctype: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            removeEmptyElements: true,
+            removeRedundantAttributes: true,
+          }
+        : {
+            useShortDoctype: true,
+            collapseWhitespace: true,
+            maxLineLength: 120,
+            preserveLineBreaks: true,
+            removeEmptyElements: true,
+            removeRedundantAttributes: true,
+          }
+    );
   });
 
   eleventyConfig.addGlobalData("currentYear", new Date().getFullYear());
